3007c0c3e9c80adfe995f6ac5158d7a7a13c526c
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-08-06 14:21:51 +0300

    Created Country struct model

---
+++ World Countries/Country.swift
@@ -0,0 +1,15 @@
+//
+//  Country.swift
+//  World Countries
+//
+//  Created by Rinalds Domanovs on 06/08/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import Foundation
+
+struct Country {
+    var name: String
+    var capital: String
+    var flag: URL
+}
