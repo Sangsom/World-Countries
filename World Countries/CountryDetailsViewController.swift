905c1019fc87662c29007bb874dd91c481faa207
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-08-06 15:59:09 +0300

    Added CountryDetailsVC and linked to storyboard

---
+++ World Countries/CountryDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CountryDetailsViewController.swift
+//  World Countries
+//
+//  Created by Rinalds Domanovs on 06/08/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class CountryDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
