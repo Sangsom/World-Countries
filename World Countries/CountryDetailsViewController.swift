532d57afea68eab49898b4ff2bcca2df7ceb9d8f
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-08-08 10:40:43 +0300

    Adjusting flagView top position to safeArea

--- World Countries/CountryDetailsViewController.swift
+++ World Countries/CountryDetailsViewController.swift
@@ -17,7 +17,7 @@ class CountryDetailsViewController: UIViewController {
     @IBOutlet var subRegionLabel: UILabel!
     @IBOutlet var alphaCodeLabel: UILabel!
     @IBOutlet var populationLabel: UILabel!
-
+    
     // MARK: Variables
     var name: String!
     var capital: String!
@@ -26,6 +26,11 @@ class CountryDetailsViewController: UIViewController {
     var alphaCode: String!
     var population: Int!
     var flagURL: URL!
+    var flagView: MacawView!
+
+    override func viewDidLayoutSubviews() {
+        flagView.frame.origin = CGPoint(x: 0, y: self.view.safeAreaInsets.top)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,14 +53,15 @@ class CountryDetailsViewController: UIViewController {
     func loadFlag() {
         let text = try! String(contentsOf: flagURL, encoding: String.Encoding.utf8)
         let node = try! SVGParser.parse(text: text)
-        let flagView = MacawView(
+
+        flagView = MacawView(
             node: node,
             frame: CGRect(
                 x: 0,
-                y: 100,
+                y: 0,
                 width: UIScreen.main.bounds.width,
                 height: UIScreen.main.bounds.width / 2.0))
-        flagView.contentMode = .scaleAspectFit
+        flagView.contentMode = .scaleAspectFill
         self.view.addSubview(flagView)
     }
 }
